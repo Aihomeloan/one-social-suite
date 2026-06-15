4006360593366a25b7a18635320307653fcc472e
Steve Fischer <s.fischer1164@gmail.com>
2026-06-15 09:01:42 -0700

    Fix: remove nested repo + green widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,12 +4,6 @@ import 'package:one_social_suite/app.dart';
 
 void main() {
   testWidgets('App boots to the 5-tab shell', (WidgetTester tester) async {
-    // Give the test a realistic phone-sized surface.
-    tester.view.physicalSize = const Size(1170, 2532);
-    tester.view.devicePixelRatio = 3.0;
-    addTearDown(tester.view.resetPhysicalSize);
-    addTearDown(tester.view.resetDevicePixelRatio);
-
     await tester.pumpWidget(const OneSocialSuiteApp());
     await tester.pumpAndSettle();
 
