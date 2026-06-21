343d98948e1751b042f766059b82bf2d84926f34
Steve Fischer <s.fischer1164@gmail.com>
2026-06-21 13:48:51 -0700

    Update splash to new 1Social branding, bump hold time to 5 seconds

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -29,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen>
     );
     _fade = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
     _controller.forward();
-    _timer = Timer(const Duration(milliseconds: 4000), _goToApp);
+    _timer = Timer(const Duration(milliseconds: 5000), _goToApp);
   }
 
   void _goToApp() {
